9b15edda96ec72f50bc11b79d8e9ca42c98edeea
Nick C <pokemonnekitgod@gmail.com>
2021-08-04 17:10:46 +0200

    sm6250-common: Changes for Pixel thermal HAL
    Change-Id: Id5e4ec3e15f047a6ffd84af34dd9943fa70f7554
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- common.mk
+++ common.mk
@@ -427,6 +427,9 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.pixel \
     thermal_symlinks
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
