0220847edc4d35c93f8cb1ce5412bc1fb0cd640a
Giovanni Ricca <giovanniricca@proton.me>
2023-11-30 18:23:43 +0100

    sm6250-common: Build fpc interface from source
    Change-Id: I80dd66139d1e53873beb14f7937cbfb721e582ca

--- atoll.mk
+++ atoll.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi
 
 PRODUCT_PACKAGES += \
+    com.fingerprints.extension@1.0.vendor \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
 # FM
