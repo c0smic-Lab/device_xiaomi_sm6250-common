9638b9dd32380ef034bb4011f8d03ef672648fe0
Kelvin Zhang <zhangkelvin@google.com>
2024-06-25 16:57:21 -0700

    sm6250-common: Explicitly set as an non-AB target
    We will be changing AB to be the default, existing non-AB targets
    need to explicitly set AB_OTA_UPDATER to false
    
    Test: th
    Change-Id: I86a76296937a6abab1b4495cd66a35144348aafd

--- atoll.mk
+++ atoll.mk
@@ -302,6 +302,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     TimeKeep
 
+# Updater
+AB_OTA_UPDATER := false
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
