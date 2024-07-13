9589eb98b3b58890f5260cf297004791c7bff505
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-13 23:22:02 +0530

    sm6250-common: Switch to QTI Memtrack AIDL HAL
    Change-Id: Ibcbaff02fd56fbccc3a88c01a5f46a89cb212ef5

--- atoll.mk
+++ atoll.mk
@@ -89,9 +89,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display
 
 PRODUCT_PACKAGES += \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    memtrack.atoll
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     disable_configstore \
