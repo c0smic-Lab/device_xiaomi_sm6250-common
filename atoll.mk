4a0611f0b61361f7d28a3d906e32047b55ad0b33
Michael Bestas <mkbestas@lineageos.org>
2024-09-26 01:39:32 +0300

    sm6250-common: Switch to AIDL thermal HAL
    Change-Id: I6ae621194044ab66fad69b9ef997b3b347bf6120

--- atoll.mk
+++ atoll.mk
@@ -295,7 +295,7 @@ PRODUCT_BOOT_JARS += \
 
 # Thermal HAL
 PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti
+    android.hardware.thermal-service.qti
 
 # TimeKeep
 PRODUCT_PACKAGES += \
