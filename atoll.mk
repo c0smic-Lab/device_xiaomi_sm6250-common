a68514126f13c51f0641c204443ab337da805031
LuK1337 <priv.luk@gmail.com>
2024-12-25 23:42:18 +0100

    sm6250-common: Switch to NCM USB tethering
    Change-Id: I20b6b2b2e26a45d8b28c7483afc83ad4da4b8643

--- atoll.mk
+++ atoll.mk
@@ -152,6 +152,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    NcmTetheringOverlay \
     SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250Settings \
