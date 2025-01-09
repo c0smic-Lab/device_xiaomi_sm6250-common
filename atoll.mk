d70b11c30fbff1cb0025c90c125da51213baae49
basamaryan <basam.aryan@gmail.com>
2025-01-09 15:56:49 -0500

    sm6250-common: media: Import Codec2 configs
    From sunfish TQ3A.230805.001.S1
    
    Change-Id: Ie2ecfa912e0fe0892a2e6b8ddc3a6b6aa3213d40

--- atoll.mk
+++ atoll.mk
@@ -152,6 +152,8 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
+    media_codecs_c2.xml \
+    media_codecs_performance_c2.xml \
     media_codecs_performance.xml \
     media_codecs.xml \
     media_profiles_V1_0.xml
