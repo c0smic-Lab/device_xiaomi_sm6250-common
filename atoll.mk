1e452b5e19e5736282e9ead9ee22fa644244be50
LuK1337 <priv.luk@gmail.com>
2025-01-17 17:06:51 +0100

    sm6250-common: Switch to common lights HAL
    Change-Id: If515e7c3f6e5b6895076f1d8381fb2c22bd1b3de

--- atoll.mk
+++ atoll.mk
@@ -144,7 +144,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.light-service.lineage
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
