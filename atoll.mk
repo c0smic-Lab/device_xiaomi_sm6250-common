b639370089f876f0199c9090a18811d958ebf72f
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    sm6250-common: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- atoll.mk
+++ atoll.mk
@@ -342,9 +342,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # Inherit proprietary targets
 $(call inherit-product, vendor/xiaomi/sm6250-common/sm6250-common-vendor.mk)
